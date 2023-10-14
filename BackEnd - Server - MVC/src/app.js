2c10177a3064f5c0fdf918e8f922d3b0b260cf60
me <me@gmail.com>
2023-10-14 10:04:08 +0200

    eighth commit: CORS

--- BackEnd - Server - MVC/src/app.js
+++ BackEnd - Server - MVC/src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));//Middleware Function to remove Access-Control-Allow-Origin
 app.use(express.json());//Middleware Function
 app.use(planetsRouter);//Middleware Function
 

a438baca56ec975b788931dba8ea43ba66e41afe
me <me@gmail.com>
2023-10-17 09:27:55 +0200

    eleventh commit: serving React front end in prod

--- BackEnd - Server - MVC/src/app.js
+++ BackEnd - Server - MVC/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router')
@@ -7,7 +8,11 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));//Middleware Function to remove Access-Control-Allow-Origin
 app.use(express.json());//Middleware Function
+app.use(express.static(path.join(__dirname, '..', 'public')));//Middleware function to run the react frontend build directly from server
 app.use(planetsRouter);//Middleware Function
 
+app.get('/', (res,req) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;

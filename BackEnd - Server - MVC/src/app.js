2a4fa318eb2dfa38a58eb749c54abde45d5182cd
me <me@gmail.com>
2023-10-17 17:17:10 +0200

    twelvth commit: loggin requests with morgan

--- BackEnd - Server - MVC/src/app.js
+++ BackEnd - Server - MVC/src/app.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const planetsRouter = require('./routes/planets/planets.router')
+const planetsRouter = require('./routes/planets/planets.router');
+const morgan = require('morgan');
 
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));//Middleware Function to remove Access-Control-Allow-Origin
+app.use(morgan('combined'));//Middleware function to call morgan log manage package
 app.use(express.json());//Middleware Function
 app.use(express.static(path.join(__dirname, '..', 'public')));//Middleware function to run the react frontend build directly from server
 app.use(planetsRouter);//Middleware Function
